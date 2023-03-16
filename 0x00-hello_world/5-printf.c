78a874b4fdcc5b1b8e4a6d7d9658ab277ee1be11
malakhbiyel <malak_hbiyel@outlook.fr>
2023-03-16 13:40:57 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,16 @@
-#!/bin/bash
-gcc -E $CFILE -o c
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints "with proper grammar, but the outcome is a piece of
+ * art,"
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
